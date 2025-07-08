da6cb3b40e1610c7c028619bd2940cc4b3ed7e71
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 15:37:01 -0300

    refatorando.

--- dados.py
+++ dados.py
@@ -1,62 +1,59 @@
 import random
 
-alvo = 10
-tentativas = 1
-jogador1_pontos = 0
-jogador2_pontos = 0
-# saída para o usuário
-print("→ Você tem 3 rodadas para somar o resultado dos lançamentos do dado.")
-print(f"→ Seu objetivo é alcançar uma soma maior que os pontos do seu adversário para ganhar.")
-
-while True:
-    try:
-        escolha = int(input("Escolha:\n1. Jogar\n2. Sair do jogo\nOpção:"))
-        if escolha == 1:
-            while tentativas <= 3:
-                print(f"Você está na {tentativas}º tentativa")
-                # entrada do jogador
-                jogador1 = input("1º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
-                # verificação
-                while jogador1 != "lançar":
-                    print("Você não digitou corretamente")
-                    jogador1 = input("1º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
-                else:
-                    # jogando dado
-                    numero_dado = random.randint(1,6)
-                    print(f"Resultado do lançamento: {numero_dado}")
-                    # somando aos pontos
-                    jogador1_pontos += numero_dado
-                    print(f"Sua pontuação atual é: {jogador1_pontos}")
-
-                jogador2 = input("2º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
-                # verificação
-                while jogador2 != 'lançar':
-                    print("Você não digitou corretamente")
-                    jogador2 = input("2º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
-                else:
-                    # jogando dado
-                    numero_dado = random.randint(1,6)
-                    print(f"Resultado do lançamento: {numero_dado}")
-                    # somando aos pontos
-                    jogador2_pontos += numero_dado
-                    print(f"Sua pontuação atual é: {jogador2_pontos}")
-                tentativas += 1
+def main():
+    pontos_jogador1 = 0
+    pontos_jogador2 = 0
+    # saída para o usuário
+    print("→ Você tem 3 rodadas para somar o resultado dos lançamentos do dado.")
+    print("→ Seu objetivo é alcançar uma soma maior que os pontos do seu adversário para ganhar.")
+    while True:
+        try:
+            escolha = int(input("Escolha:\n1. Jogar\n2. Sair do jogo\nOpção:"))
+            if escolha == 1:
+                jogar_rodada(pontos_jogador1,pontos_jogador2)
             else:
-                # verificação
-                if jogador1_pontos > jogador2_pontos:
-                    print("Jogador 1, você ganhou!")
-                    print(f"Seus pontos foram: {jogador1_pontos}")
-                elif jogador2_pontos > jogador1_pontos:
-                    print("Jogador 2, você ganhou!")
-                    print(f"Seus pontos foram: {jogador2_pontos}")
-                else:
-                    print("Houve um empate!")
-                tentativas = 1
-                jogador1_pontos = 0
-                jogador2_pontos = 0
-        else:
-            print("Obrigado. Até logo!")
-            break
-    except ValueError:
-        print("Somente aceita dígitos")
+                print("Obrigado. Até logo!")
+                break
+        except ValueError:
+            print("Somente aceita dígitos")
     
+def jogar_rodada(pontos_jogador1,pontos_jogador2):
+    tentativas = 1
+    while tentativas <= 3:
+        print(f"Você está na {tentativas}º tentativa\n")
+        # entrada do jogador
+        jogador1 = input("1º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
+        pontos_jogador1 = lancar_dados(jogador1,pontos_jogador1)
+        # entrada do jogador
+        jogador2 = input("2º jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
+        pontos_jogador2 = lancar_dados(jogador2,pontos_jogador2)
+        # acrescentando tentativas
+        tentativas += 1
+    else:
+        # verificação
+        if pontos_jogador1 > pontos_jogador2:
+            print("Jogador 1, você ganhou!")
+            print(f"Seus pontos foram: {pontos_jogador1}")
+        elif pontos_jogador2 > pontos_jogador1:
+            print("Jogador 2, você ganhou!")
+            print(f"Seus pontos foram: {pontos_jogador2}")
+        else:
+            print("Houve um empate!")
+    tentativas = 1
+    pontos_jogador1 = 0
+    pontos_jogador2 = 0
+
+def lancar_dados(jogador,pontos_jogador):
+    while jogador != "lançar":
+        print("Você não digitou corretamente")
+        jogador = input("Jogador, digite 'Lançar' para lançar o dado: ").strip().lower()
+    else:
+        # jogando dado
+        numero_dado = random.randint(1,6)
+        print(f"Resultado do lançamento: {numero_dado}")
+        # somando aos pontos
+        pontos_jogador += numero_dado
+        print(f"Sua pontuação atual é: {pontos_jogador}\n")
+    return pontos_jogador
+
+main()
