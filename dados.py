fe82d81b6f4a1c69cc24a86bd2d7932fd5ef985c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 14:03:09 -0300

    entrada do jogador, jogando dado e somando aos pontos

--- dados.py
+++ dados.py
@@ -1,7 +1,25 @@
 import random
-# 1 - entrada do usuário
-entrada_usuario = "Jogar" #input("Digite 'Jogar' para jogar o dado: ")
 
-if entrada_usuario == "Jogar":
-    dado = random.randint(1,6)
-    print(dado)
+alvo = 15
+tentativas = 1
+total_pontos = 0
+# saída para o usuário
+print("→ Você tem 3 rodadas para somar o resultado dos lançamentos do dado.")
+print("→ Seu objetivo é alcançar uma soma maior que o número alvo para ganhar.")
+
+while tentativas <= 3:
+    print(f"Você está na {tentativas}º tentativa")
+    # entrada do jogador
+    jogador1 = input("Digite 'Lançar' para lançar o dado: ").strip()
+    if jogador1 == "Lançar":
+        # jogando dado
+        numero_dado = random.randint(1,6)
+        print(f"Resultado do lançamento: {numero_dado}")
+        # somando aos pontos
+        total_pontos += numero_dado
+    tentativas += 1
+
+if total_pontos >= alvo:
+    print("Você ganhou!")
+else:
+    print("Você perdeu!")
